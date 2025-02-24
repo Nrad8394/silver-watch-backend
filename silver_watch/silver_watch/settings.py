9a27120741aebcf0978d9cf07b33cb8b5bb5e83e
Nrad8394 <94044917+Nrad8394@users.noreply.github.com>
2025-02-24 09:21:31 +0300

    update

--- silver_watch/silver_watch/settings.py
+++ silver_watch/silver_watch/settings.py
@@ -320,7 +320,7 @@ REST_AUTH = {
     'JWT_AUTH_HTTPONLY': True,
     'JWT_AUTH_SAMESITE': "None",
     'JWT_AUTH_RETURN_EXPIRATION': False,
-    'JWT_AUTH_COOKIE_USE_CSRF': False,
+    'JWT_AUTH_COOKIE_USE_CSRF': True,
     'JWT_AUTH_COOKIE_ENFORCE_CSRF_ON_UNAUTHENTICATED': False,
 }
 SIMPLE_JWT = {
@@ -345,7 +345,6 @@ SIMPLE_JWT = {
     "COOKIE_SAMESITE": "None",  # Allows cross-site requests
     "COOKIE_PATH": "/",  # Allows the cookie to be sent to any path
     
-
     "AUTH_HEADER_TYPES": ("Bearer",),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
     "USER_ID_FIELD": "id",
